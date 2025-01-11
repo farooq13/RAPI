b83be8dea5b48af0c1c43e9ab19bfff3d9accc8a
farooq13 <farukidris19@gmail.com>
2025-01-11 17:00:20 +0100

    add products app

--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -3,5 +3,6 @@ import requests
 # endpoint = 'https://httpbin.org/status/200/'
 endpoint = 'http://127.0.0.1:8000/api/'
 
-get_respose = requests.get(endpoint)
-print(get_respose.text)
+get_response = requests.get(endpoint, json={"query": "Hello world"})
+
+print(get_response.json())
